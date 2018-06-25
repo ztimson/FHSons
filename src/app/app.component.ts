3ad6de5b6c601f05084b71deeca243c5ead03265
Zak Timson <zaktimson@gmail.com>
2018-06-25 11:19:17 -0400

    Added unit conversions and formula scaling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import {Component} from '@angular/core';
+import {AngularFirestore} from 'angularfire2/firestore';
 import {share} from 'rxjs/operators';
+import {ConvertFromGPipe, ConvertToGPipe} from './units.pipe';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +13,28 @@ export class AppComponent {
   formula;
   components;
   unit = 'g';
-  newTotal: number = 0;
+
+  _newTotal: number = 0;
+  get newTotal() {
+    return new ConvertFromGPipe().transform(this._newTotal, this.unit);
+  }
+  set newTotal(total) {
+    this._newTotal = new ConvertToGPipe().transform(total, this.unit);
+  }
 
   constructor(private db: AngularFirestore) {
     this.formulas = this.db.collection('formulas').valueChanges();
   }
 
   displayFormula(formula) {
-    formula.components.map(row => row.component = this.db.doc(`components/${row.component.id}`).valueChanges().pipe(share()));
-    formula.total = formula.components.reduce((acc, row) => acc += row.quantity, 0);
+    formula.components.map(
+      row =>
+        (row.component = this.db
+          .doc(`components/${row.component.id}`)
+          .valueChanges()
+          .pipe(share()))
+    );
+    formula.total = formula.components.reduce((acc, row) => (acc += row.quantity), 0);
     this.newTotal = formula.total;
     this.formula = formula;
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
-import { environment } from '../environments/environment.prod';
+import {AppComponent} from './app.component';
+import {environment} from '../environments/environment.prod';
 import {ConvertFromGPipe, ConvertToGPipe} from './units.pipe';
-import { ScalePipe } from './scale.pipe';
+import {ScalePipe} from './scale.pipe';
+import {AngularMaterialModule} from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ConvertFromGPipe,
-    ConvertToGPipe,
-    ScalePipe
-  ],
+  declarations: [AppComponent, ConvertFromGPipe, ConvertToGPipe, ScalePipe],
   imports: [
+    AngularMaterialModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
