e3ebe48f683a4fbcd94562365e2c703e29c444f8
Zak Timson <zaktimson@gmail.com>
2018-07-11 21:08:53 -0400

    Redirect to formula manager if in electron app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ElectronService} from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router, public electron: ElectronService) {}
+
+  ngOnInit() {
+    if(this.electron.isElectronApp) {
+      this.router.navigate(['/formulaManager']);
+    }
+  }
+}
