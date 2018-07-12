cabb4eb2f3894fa1aac0dafb15f3eb5b77fc9379
Zak Timson <zaktimson@gmail.com>
2018-07-11 22:41:41 -0400

    Made footer slightly better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {ElectronService} from 'ngx-electron';
+import {AngularFirestore} from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router, public electron: ElectronService) {}
+  categories;
+
+  constructor(private router: Router, private db: AngularFirestore, public electron: ElectronService) {
+    this.categories = this.db.collection('categories').valueChanges();
+  }
 
   ngOnInit() {
-    if(this.electron.isElectronApp) {
+    if (this.electron.isElectronApp) {
       this.router.navigate(['/formulaManager']);
     }
   }
