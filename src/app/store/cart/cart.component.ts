91e40929e0c41c5112670979cc10026f77d9bcb0
Zak Timson <zaktimson@gmail.com>
2018-07-19 23:03:57 -0400

    Styled cart page

--- src/app/store/cart/cart.component.ts
+++ src/app/store/cart/cart.component.ts
@@ -1,14 +1,30 @@
 import {Component} from '@angular/core';
 import {LocalStorage} from 'webstorage-decorators';
+import {access} from 'fs';
 
 @Component({
-    selector: 'cart',
-    templateUrl: 'cart.component.html'
+  selector: 'cart',
+  templateUrl: 'cart.component.html'
 })
 export class CartComponent {
-    @LocalStorage({defaultValue: []}) cart: {id: string, name: string, cost: number, quantity: number}[]; 
+  @LocalStorage({defaultValue: []})
+  cart: {id: string; item: string; price: number; quantity: number}[];
 
-    constructor() {
+  address1: string;
+  address2: string;
+  city: string;
+  province: string;
+  postal: string;
 
-    }
+  constructor() {}
+
+  remove(i: number) {
+    let c = this.cart;
+    c.splice(i, 1);
+    this.cart = c;
+  }
+
+  total() {
+    return this.cart.reduce((acc, row) => acc + row.price * row.quantity, 0);
+  }
 }
